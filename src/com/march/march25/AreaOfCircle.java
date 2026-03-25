fa019c3273d71fc647e6e6cdaaeffefdd3e75455
rushikesh <redrock7823@gmail.com>
2026-03-25 18:03:05 +0530

    Find Area

--- src/com/march/march25/AreaOfCircle.java
+++ src/com/march/march25/AreaOfCircle.java
@@ -4,7 +4,7 @@ public class AreaOfCircle {
     public static void main(String[] args) {
         double radius = 4;
 
-        double sum = 3.14159 * radius * radius;
+        double sum = 3.14159 * radius * radius;  // formula - area of circle = 3.24 * radius^
         System.out.println("Area of Circle is "+sum);
     }
 }
---
+++ src/com/march/march25/AreaOfRectange.java
@@ -0,0 +1,11 @@
+package com.march.march25;
+
+public class AreaOfRectange {
+    public static void main(String[] args) {
+        int length = 8;
+        int width = 6;
+
+        int area = length * width; // formula - Area of rectangle = length * width
+        System.out.println("Area of rectange is "+area);
+    }
+}
