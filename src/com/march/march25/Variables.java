ade5694b23f4d60c1358e006066ba83d77c0f5f0
rushikesh <redrock7823@gmail.com>
2026-03-25 17:41:11 +0530

    corejava changes

---
+++ src/com/march/march25/Variables.java
@@ -0,0 +1,24 @@
+package com.march.march25;
+
+public class Variables {
+    public static void main(String[] args) {
+        int a = 23;
+        short b = 2;
+        byte c = 3;
+        char d = 'r';
+        long e = 2345L;
+        float f = 65.98f;
+        double g = 987.977d;
+        boolean h = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+
+    }
+}
