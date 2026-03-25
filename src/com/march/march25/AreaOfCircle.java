5364625c0077fc2a4ebb439a7ee54ee2767c1b5b
rushikesh <redrock7823@gmail.com>
2026-03-25 17:55:18 +0530

    Find the number is even or odd

---
+++ src/com/march/march25/AreaOfCircle.java
@@ -0,0 +1,10 @@
+package com.march.march25;
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        double radius = 4;
+
+        double sum = 3.14159 * radius * radius;
+        System.out.println("Area of Circle is "+sum);
+    }
+}
