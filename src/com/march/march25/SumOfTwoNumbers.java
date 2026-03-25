8576283973930ad93a419007341b2be0e3d03260
rushikesh <redrock7823@gmail.com>
2026-03-25 17:57:50 +0530

    Find Sum of Two numbers

---
+++ src/com/march/march25/SumOfTwoNumbers.java
@@ -0,0 +1,12 @@
+package com.march.march25;
+
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+
+        int a = 23;
+        int b = 25;
+
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
