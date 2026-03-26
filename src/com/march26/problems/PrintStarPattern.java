e8979b0483c58a4c1cfccffa31b3b7bef360823c
rushikesh <redrock7823@gmail.com>
2026-03-26 15:29:33 +0530

    Print Star pattern

---
+++ src/com/march26/problems/PrintStarPattern.java
@@ -0,0 +1,14 @@
+package com.march26.problems;
+// Print the following star pattern (right triangle, 5 rows)
+
+public class PrintStarPattern {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++){
+          for (int j = 1; j <= i; j++) {
+            System.out.print("* ");
+        }
+            System.out.println();
+        }
+    }
+}
